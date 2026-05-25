478ee8c9efbee19200fb76566b0111190b524428
Joe Tilley <joewilliamtilley@gmail.com>
2026-05-25 12:57:49 +0100

    created initial data lists

--- The Borderless Tourist.py
+++ The Borderless Tourist.py
@@ -1,1 +1,5 @@
 #The Borderless Tourist
+
+destinations = ["Paris, France", "Shanghai, China", "Los Angeles, USA", "Sao Paulo, Brazil", "Cairo, Egypt"]
+
+test_traveler = ['Erin Wilkes', 'Shanghai, China', ['historical site', 'art']]
